76f9062f2973aca0b15762e7f958fd4d56d12495
Roman Kazarov <rkazarov@siriusiq.com>
2022-09-08 23:04:05 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(NumJewelsInStones("aA", "aAAbbbb"));
+        Console.WriteLine(NumJewelsInStones("z", "ZZ"));
+    }
+
+    public static int NumJewelsInStones(string jewels, string stones) {
+        jewels.ToHashSet();
+        int result = 0;
+        for (int i = 0; i < stones.Length; i++)
+        {
+            if(jewels.Contains(stones[i])) result = result + 1;
+        }        
+        return result;
+    }
+}
